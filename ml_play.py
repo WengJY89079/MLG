2cff33c0cde2a18ef095efa3895dca6bc67aaa4f
WengJY89079 <WengJY89079@gmail.com>
2020-03-26 15:19:10 +0800

    if elif

--- ml_play.py
+++ ml_play.py
@@ -22,6 +22,12 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
+    
+    BallX = -1
+    BallY = -1
+    BallNewX = -1
+    BallNewY = -1
+    downX = -1
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
@@ -30,7 +36,6 @@ def ml_loop():
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
-
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
@@ -43,10 +48,37 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
-
+        platform = scene_info.platform[0]
+        if(BallX == -1):
+            BallX,BallY = scene_info.ball
+        else:
+            BallNewX,BallNewY = scene_info.ball
+            nowVec = BallNewX-BallX,BallNewY-BallY
+            BallX = BallNewX
+            BallY = BallNewY
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            if(nowVec[1] < 0):
+                comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            elif(nowVec[0] !=0):
+                ins = nowVec[0]
+                downStep = int((400 - BallNewY) / nowVec[1])
+                #print(downStep)
+                downX = BallNewX + int(downStep * ins)
+                #print(downX)
+                while(downX < 0 or downX > 200):
+                    if(downX < 0):
+                        downX = -downX
+                    else:
+                        downX = 400 - downX
+                #print(downX)
+                if(scene_info.platform[0] + 20 > downX):
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                elif(scene_info.platform[0] - 20 < downX):
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.NONE)
+            #comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
